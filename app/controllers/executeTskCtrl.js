7feb1b4d329ff1b0bf1860a36c5068684a48f9e5
Akshayakt <akshaya@qburst.com>
2016-04-06 14:44:01 +0530

    removed console log

--- app/controllers/executeTskCtrl.js
+++ app/controllers/executeTskCtrl.js
@@ -2,7 +2,6 @@ angular.module('QCrowdPro').controller('executeTskCtrl',function ($scope,steps) 
 
 steps.$promise.then(function (data) {
   $scope.steps = data;
-  console.log(data);
 },function (error) {
   console.log(error);
 });
